43bd7a0853e39460ed21902c5df2a7a37f691ff9
codingwithrih <rihashehzadi2003@gmail.com>
2025-06-19 15:21:59 +0500

    Chef-Claude-Setup

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+        <header>
+            <img src="../src/assets/chef-claude-icon.png" alt="logo" className="header-logo" />
+            <h1 className="header-title">Chef Claude</h1>
+        </header>
+        </>
+    );
+}
